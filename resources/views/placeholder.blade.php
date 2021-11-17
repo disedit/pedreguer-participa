d8e1c60a6edee549c268ce2d7bf35dad2d169a1f
Guillem <guillemch@users.noreply.github.com>
2021-11-17 09:53:03 +0100

    Update placeholder message

--- resources/views/placeholder.blade.php
+++ resources/views/placeholder.blade.php
@@ -6,7 +6,7 @@
         <div class="placeholder results-pending">
             <i class="far fa-comment-alt" aria-hidden="true"></i>
             <h2>@lang('participa.voting_closed')</h2>
-            <p>@lang('participa.awaiting_results', ['publish_date' => human_date($edition->publish_results), 'publish_time' => date('H:i', strtotime($edition->publish_results))])</h2>
+            <p>Els resultats es faran públics en aprovar-los el Consell de Participació</h2>
         </div>
     </div>
     <div class="col-md-4">
