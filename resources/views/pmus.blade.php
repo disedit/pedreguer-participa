7cea54fefd13dcbb95328ec99f79950c18b2bc70
guillem <guillem@disedit.com>
2022-10-04 19:26:08 +0200

    Fix typos

--- resources/views/pmus.blade.php
+++ resources/views/pmus.blade.php
@@ -31,8 +31,8 @@
         <p>Mitjançant aquesta web, vosté podrà conéixer tots els detalls del projecte d'elaboració del <strong>PMUS de la població de Pedreguer</strong>.</p>
         <p>Un <strong>Pla de Mobilitat Urbana Sostenible</strong> (PMUS) és un conjunt d'actuacions que tenen com a objectiu la potenciació de les formes de desplaçament més sostenibles <strong>(caminar, bicicleta i transport públic)</strong> dins d'una aglomeració urbana; és a dir, d’aquells modes de transport que compatibilitzen amb el creixement econòmic, cohesió social i defensa del medi ambient, garantint, d'aquesta manera, una millor qualitat de vida per als ciutadans. </p>
         <p>A més podrà col·laborar en el projecte realitzant l'<a href="{{ $generalSurvey }}" target="_blank">enquesta en línia</a>. Els seus suggeriments són molt importants per a nosaltres atés que ens ajuden a conéixer l'opinió de les persones i així poder millorar, així que no dubte a utilitzar la <a href="" target="_blank">bústia de suggeriments</a>.</p>
-        <p>El punt de sortida de la <strong>Mobilitat Sostenible</strong> es centra en conèixer el grau de sostenibilitat dels diferents modes de transport disponibles per a cobrir la actual demanda de desplaçaments i donar solucions a la distribució urbana de mercaderies.</p>
-        <p>Les bases d’una mobilitat més sostenible es constitueixen en la recerca de soluciones per a reduir l’ús dels recursos naturals no renovables i que provoquen menys patiments al medi ambient.</p>
+        <p>El punt de sortida de la <strong>Mobilitat Sostenible</strong> es centra en conèixer el grau de sostenibilitat dels diferents modes de transport disponibles per a cobrir l'actual demanda de desplaçaments i donar solucions a la distribució urbana de mercaderies.</p>
+        <p>Les bases d’una mobilitat més sostenible es constitueixen en la recerca de solucions per a reduir l’ús dels recursos naturals no renovables.</p>
         
         <div class="buttons d-lg-none">
             <a href="{{ $generalSurvey }}" class="huge-button main">
